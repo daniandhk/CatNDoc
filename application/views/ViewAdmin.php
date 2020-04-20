442e78d46a2b4ef558449aae4d4c8e5bfb12d982
Fariz <frzrzky@gmail.com>
2020-04-20 18:30:04 +0700

    Fix ViewAdmin,php simple
    Memperbaiki tampilan

--- application/views/ViewAdmin.php
+++ application/views/ViewAdmin.php
@@ -9,52 +9,34 @@
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
 
     <title>VIEW ADMIN</title>
-    <style>
-    table, th, td {
-        border: 3px solid black;
-        border-collapse: collapse;
-    }
-    th, td {
-        padding: 5px;
-        text-align: left;    
-    }
 
-    #btn1{
-        margin-left:3%;
-    }
-    #btn2{
-        margin-left:30%;
-    }
-
-    #btn3{
-        margin-left:75%;
-        margin-top:-3%;
-    }
-</style>
-
-<script>
-$(document).ready(function(){
-  $("#tblregister").hide;
-  $("#tblhewan").hide;
-  $("#tblaponmen").hide;
-  
-  
-  $("#btn1").click(function(){
-    $("#tblregister").toggle();
-  });
+	<script>
+	$(document).ready(function(){
+	  $("#tblregister").hide;
+	  $("#tblhewan").hide;
+	  $("#tblaponmen").hide;
+	  
+	  $("#btn1").click(function(){
+	    $("#tblregister").toggle();
+	  });
 
 
-  $("#btn2").click(function(){
-    $("#tblhewan").toggle();
-  });
-  
+	  $("#btn2").click(function(){
+	    $("#tblaponmen").toggle();
+	  });
+	  
 
-  $("#btn3").click(function(){
-    $("#tblaponmen").toggle();
-  });
-  
-});
-</script>
+	  $("#btn3").click(function(){
+	    $("#tblhewan").toggle();
+	  });
+	  
+	});
+	</script>
+	<style type="text/css">
+		#tblregister, #tblaponmen, #tblhewan {
+			display: none;
+		}
+	</style>
   </head>
   <body>
   <a class="nav-link" href="<?= site_url('HomePage') ?>">Home <span class="sr-only">(current)</span></a>
@@ -62,86 +44,117 @@ $(document).ready(function(){
    <br>
    <br>
 
-    <button id="btn1" class="btn btn-secondary">DATA REGISTER</button>
+   	<div class="container">
+   		<div class="row">
+   			<div class="col-3">
+   				<button id="btn1" class="btn btn-secondary my-1">DATA REGISTER</button>
+   				<button id="btn2" class="btn btn-secondary my-1">DATA APPOINMENT</button> 
+   				<button id="btn3" class="btn btn-secondary my-1">DATA HEWAN</button>
+   			</div>
+   			<div class="col-9">
+   				<div id="tblregister">
+   					<div class="card">
+  						<div class="card-header">
+    						DATA REGISTER
+  						</div>
+  						<div class="card-body">
+    						<table class="table table-hover table-bordered" style="">
+						    	<thead>
+						    		<tr>
+										<th>Nama</th>
+										<th>Email</th>
+										<th>Password</th>
+									</tr>
+						    	</thead>
+							    <?php 
+								//$no = 1;
+								//foreach($user as $u){ 
+								?>
+								<tr>
+									<td><?php //echo  ?></td>
+									<td><?php //echo  ?></td>
+									<td>
+									      <?php //echo EDIT; ?>
+						                              <?php //echo HAPUS ?>
+									</td>
+								</tr>
+								<?php//  } ?>
+							</table>
+						</div>
+					</div>	    
+   				</div>
+
+   				<div class="my-2" id="tblaponmen">
+   					<div class="card">
+  						<div class="card-header">
+    						DATA APPOINMENT
+  						</div>
+  						<div class="card-body">
+    						<table class="table table-hover table-bordered" style="">
+						    	<thead>
+						    		<tr>
+										<th>Nama</th>
+										<th>Email</th>
+										<th>No.Telp</th>
+							            <th>Date</th>
+							            <th>Jenis Hewan</th>
+							            <th>Keluhan</th>
+									</tr>
+						    	</thead>
+									
+							    <?php 
+								//$no = 1;
+								//foreach($user as $u){ 
+								?>
+								<tr>
+									<td><?php //echo  ?></td>
+									<td><?php //echo  ?></td>
+						            <td><?php //echo  ?></td>
+						            <td><?php //echo  ?></td>
+						            <td><?php //echo  ?></td>
+						            <td><?php //echo  ?></td>
+								</tr>
+								<?php//  } ?>
+							</table>
+						</div>
+					</div>	     
+   				</div>
+
+   				<div class="my-2" id="tblhewan">
+   					<div class="card">
+  						<div class="card-header">
+    						DATA HEWAN
+  						</div>
+  						<div class="card-body">
+    						<table class="table table-hover table-bordered" style="">
+			    				<thead>
+			    					<tr>
+										<th>Jenis</th>
+										<th>Berat</th>
+										<th>Tinggi</th>
+							            <th>Warna</th>
+									</tr>
+			    				</thead>
+							    <?php 
+								//$no = 1;
+								//foreach($user as $u){ 
+								?>
+								<tr>
+									<td><?php //echo  ?></td>
+									<td><?php //echo  ?></td>
+									<td><?php //echo  ?></td>
+						            <td><?php //echo  ?></td>
+								</tr>
+								<?php//  } ?>
+							</table>
+						</div>
+					</div>			    			
+   				</div>
+   			</div>
+   		</div>
+   	</div>
     
-    <button id="btn2" class="btn btn-secondary">DATA HEWAN</button>
     
-    <button id="btn3" class="btn btn-secondary">DATA APPOINMENT</button> 
-
-    <h2 style="position:absolute;left:0.5%;top:5%;">DATA REGISTER</h2>
-    <table id="tblregister" style="position:absolute;top:15%;left:1%;display:none;">
-		<tr>
-			<th>Nama</th>
-			<th>Email</th>
-			<th>Password</th>
-		</tr>
-	    <?php 
-		//$no = 1;
-		//foreach($user as $u){ 
-		?>
-		<tr>
-			<td><?php //echo  ?></td>
-			<td><?php //echo  ?></td>
-			<td>
-			      <?php //echo EDIT; ?>
-                              <?php //echo HAPUS ?>
-			</td>
-		</tr>
-		<?php//  } ?>
-	</table>
-
-    <h2 style="text-align:center;position:absolute;left:38%;top:5%">DATA HEWAN</h2>
-    <table id="tblhewan" style="position:absolute;right:50%;top:15%;display:none;">
-		<tr>
-			<th>Jenis</th>
-			<th>Berat</th>
-			<th>Tinggi</th>
-            <th>Warna</th>
-		</tr>
-	    <?php 
-		//$no = 1;
-		//foreach($user as $u){ 
-		?>
-		<tr>
-			<td><?php //echo  ?></td>
-			<td><?php //echo  ?></td>
-			<td><?php //echo  ?></td>
-            <td><?php //echo  ?></td>
-			      <?php //echo EDIT; ?>
-                              <?php //echo HAPUS ?>
-			</td>
-		</tr>
-		<?php//  } ?>
-	</table>
-
-    <h2 style="text-align:center;position:absolute;right:14%;top:5%">DATA APPOINMENT</h2>
-    <table id="tblaponmen" style="position:absolute;right:10%;top:15%;display:none;">
-		<tr>
-			<th>Nama</th>
-			<th>Email</th>
-			<th>No.Telp</th>
-            <th>Date</th>
-            <th>Jenis Hewan</th>
-            <th>Keluhan</th>
-		</tr>
-	    <?php 
-		//$no = 1;
-		//foreach($user as $u){ 
-		?>
-		<tr>
-			<td><?php //echo  ?></td>
-			<td><?php //echo  ?></td>
-            <td><?php //echo  ?></td>
-            <td><?php //echo  ?></td>
-            <td><?php //echo  ?></td>
-            <td><?php //echo  ?></td>
-		
-			      <?php //echo EDIT; ?>
-                              <?php //echo HAPUS ?>
-			</td>
-		</tr>
-		<?php//  } ?>
-	</table>
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
     <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
