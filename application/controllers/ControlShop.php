f938bb33c35d6bb4c4b3bf62662009dd3c1a1d74
annisyahs <63383485+annisyahs@users.noreply.github.com>
2020-04-22 21:28:55 +0700

    Update ControlShop.php

--- application/controllers/ControlShop.php
+++ application/controllers/ControlShop.php
@@ -2,13 +2,11 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 class ControlShop extends CI_Controller{
-
 	function __construct()
 	{
 		parent::__construct();
 		$this->load->model('ModelShop');
 	}
-
 	public function index()
 	{
 		$this->load->view('ViewShop');
