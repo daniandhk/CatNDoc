86f832a926bc9bbf2e3b15c09ec33c45ac4c0460
indirafsyzhr <63384787+indirafsyzhr@users.noreply.github.com>
2020-04-23 10:56:55 +0700

    Update ModelShop.php

--- application/models/ModelShop.php
+++ application/models/ModelShop.php
@@ -5,5 +5,32 @@ class ModelShop extends CI_Model{
     function __construct (){
         parent::__construct();
     }
+    
+    public function get_all(){
+		return $this->db->get('produk')->result_array();
+	}
 
+	
+	public function get_produk($id_produk){
+		$this->db->where('id_produk', $id_produk);
+		return $this->db->get('produk')->row_array();
+	}
+	
+
+	public function insert_produk($data){
+		return $this->db->insert('produk',$data);
+	}
+	
+
+	
+	public function update_produk($id_produk, $data){
+		$this->db->where('id_produk', $id_produk);
+		return $this->db->update('produk',$data);
+	}
+	
+	
+	public function delete_produk($id_produk){
+		$this->db->where('id_produk', $id_produk);
+		return $this->db->delete('produk');
+	}
 }
