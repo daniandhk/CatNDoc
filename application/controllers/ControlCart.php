e5a93697fe787734870192e0ea11bded3cb63a0e
Annisya Hayati Suhendar <63383485+annisyahs@users.noreply.github.com>
2020-04-27 03:44:50 +0700

    update navbar cart

--- application/controllers/ControlCart.php
+++ application/controllers/ControlCart.php
@@ -11,6 +11,39 @@ class ControlCart extends CI_Controller{
 
 	public function index()
 	{
+		if(isset($_SESSION['logged_in'])){
+			$data['email'] = $this->session->userdata('email');
+			$email = $this->session->userdata('email');
+			$user = $this->ModelCart->get_profile($email);
+			$data['nama'] = $user->nama;
+			$this->load->view('ViewCart', $data);
+		}
 		$this->load->view('ViewCart');
+	}
+
+	public function logout(){
+		if(isset($_SESSION['email'])){
+			unset($_SESSION['email']);
+		}
+		if(isset($_SESSION['logged_in'])){
+			unset($_SESSION['logged_in']);
+		}
+        redirect('ControlCart');
+	}
+
+	public function login(){
+	    $data = array(
+	      'email' => $this->input->post('email'),
+	      'password' => $this->input->post('password')
+	    );
+	    $login = $this->ModelCart->login($data);
+	    if($login == TRUE) {
+	      $this->session->set_userdata('email', $data['email']);
+	      $this->session->set_userdata('logged_in', TRUE);
+	      redirect('/ControlCart');
+	    }else{
+	      $this->session->set_flashdata('error_message', 'email atau password salah');
+	      redirect('/ControlCart');
+	    }
 	}
 }
--- application/models/ModelCart.php
+++ application/models/ModelCart.php
@@ -6,4 +6,25 @@ class ModelCart extends CI_Model{
         parent::__construct();
     }
 
+    public function get_profile($email){
+	    $this->db->where('email', $email);
+	    $query = $this->db->get('user');
+	    if($query->num_rows() > 0) {
+	      return $query->row();
+	    }else{
+	      return false;
+	    }
+	}
+
+    public function login($data) {
+	    $this->db->where('email', $data['email']);
+	    $this->db->where('password', $data['password']);
+	    $query = $this->db->get('user');
+	    if($query->num_rows() > 0) {
+	      return true;
+	    }else{
+	      return false;
+	    }
+	}
+
 }
